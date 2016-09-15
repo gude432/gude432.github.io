67ee40517803ff788e493b4409b9240083e84bb5
Gudeta G <gudbog@gmail.com>
2016-09-15 19:52:09 +0300

    Added google analytics code

--- main.js
+++ main.js
@@ -47,49 +47,18 @@ function filterGroup(evt, groupName) {
 
 }
 
+//google analytics
 var mybtn2 = document.getElementsByClassName("testbtn2")[0];
 mybtn2.click();
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-84205866-1', 'auto');
+  ga('send', 'pageview');
 
+</script>
 
-// // Slideshows
-// var slideIndex = 1;
-
-// function plusDivs(n) {
-    // slideIndex = slideIndex + n;
-    // showDivs(slideIndex);
-// }
-
-// function showDivs(n) {
-    // var x = document.getElementsByClassName("mySlides");
-    // if (n > x.length) {
-        // slideIndex = 1
-    // }
-    // if (n < 1) {
-        // slideIndex = x.length
-    // };
-    // for (i = 0; i < x.length; i++) {
-        // x[i].style.display = "none";
-    // }
-    // //x[slideIndex-1].style.display = "block";
-// }
-
-// showDivs(1);
-
-// // Progress Bars
-// function move() {
-    // var elem = document.getElementById("myBar");
-    // var width = 1;
-    // var id = setInterval(frame, 10);
-
-    // function frame() {
-        // if (width == 100) {
-            // clearInterval(id);
-        // } else {
-            // width++;
-            // elem.style.width = width + '%';
-            // document.getElementById("demoprgr").innerHTML = width * 1 + '%';
-        // }
-    // }
-// }
