e8634c037ac18c6336b321c2a166f1a1f1588a99
Gudeta G <gudbog@gmail.com>
2016-09-15 13:53:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function filterGroup(evt, groupName) {
 
     var activebtn = document.getElementsByClassName("testbtn2");
     for (i = 0; i < x.length + 1; i++) {
-        activebtn[i].className = activebtn[i].className.replace(" w3-theme-l1", "");
+        activebtn[i].className = activebtn[i].className.replace(" w3-theme-d1", "");
     }
     if (groupName != "all") {
         document.getElementById("Name").style.display = "block";
@@ -42,7 +42,7 @@ function filterGroup(evt, groupName) {
             x[i].style.display = "block";
         }
     }
-    evt.currentTarget.className += " w3-theme-l1";
+    evt.currentTarget.className += " w3-theme-d1";
 
 }
 
